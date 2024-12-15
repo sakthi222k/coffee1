652ee4fea37d4eed09b303c1b75ca9e662b7eded
sakthivel <148855118+sakthi222k@users.noreply.github.com>
2024-12-15 18:46:30 +0530

    Add files via upload

---
+++ client/main.js
@@ -0,0 +1,11 @@
+// YOUR JAVASCRIPT CODE FOR INDEX.HTML GOES HERE
+var sideNavbar = document.querySelector(".side-navbar");
+
+function addNavbar()
+{
+    sideNavbar.style.right = "0"
+}
+function closeNavbar()
+{
+    sideNavbar.style.right = "-60%"
+}
